ebecb206a7adda47a9ee0c838c169fc7d2314ea4
kazunori1981kazunori <kazunori1981kazunori@gmail.com>
2026-07-24 21:56:12 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="golf-oly-pay-iphone-v1";
+const CACHE="golf-oly-pay-flexible-v2";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
